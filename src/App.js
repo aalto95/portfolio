b71066ec369682b2b13af74bf4cc5c915aa6a5f8
y2k01 <stanportableuse@gmail.com>
2021-04-09 13:38:41 +0900

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Projects'
+import './components/Navbar'
+import {BrowserRouter, Route} from "react-router-dom";
+import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div>
+        <Route path="/projects"
+               render={ () => <Projects/>}/>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Projects = (props) => {
+    return (
+        <div>
+            yoooo
+        </div>
+    )
+}
+
+export default Projects
