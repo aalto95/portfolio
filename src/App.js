1095638d391401f375bdf3e809197232e22651fe
y2k01 <stanportableuse@gmail.com>
2021-04-28 17:03:14 +0900

    firebase initialized

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import logo from './logo.svg';
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import {useAuthState} from "react-firebase-hooks/auth";
+
 import './App.css';
 import './components/Projects/Projects'
 import './components/Navbar/Navbar'
@@ -8,6 +14,19 @@ import Navbar from "./components/Navbar/Navbar";
 import Contacts from "./components/Contacts/Contacts"
 import Footer from "./components/Footer/Footer"
 import Home from "./components/Home/Home";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBjdMVFjxSsi0PuIguuE1bY27u2DIh1yBw",
+    authDomain: "portfolio-f656b.firebaseapp.com",
+    projectId: "portfolio-f656b",
+    storageBucket: "portfolio-f656b.appspot.com",
+    messagingSenderId: "481293606628",
+    appId: "1:481293606628:web:45bf774f97d4ab1b41d6f1",
+    measurementId: "G-69P3QRJDV8"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 const App = props => {
   return (
