2128bd9ae7bbd52bc65155f5ac245590420fce8d
y2k01 <stanportableuse@gmail.com>
2021-06-24 00:28:25 +0900

    resume attached

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from './Resume.module.css'
 import externalLinkIcon from '../../assets/icons/external-link.png'
 import downloadIcon from '../../assets/icons/download.png'
+import resume from './resume.pdf'
 
 const Resume = props => {
     return (
@@ -9,11 +10,11 @@ const Resume = props => {
             <section className={styles.resumeSection}>
                 <h2>Resume</h2>
                 <div className={styles.linkBlock}>
-                    <a href="" className={styles.link}>
+                    <a href="https://drive.google.com/file/d/1OG-rULBRuMy8cq4a0rsxkfuJMQa-opNA/view" className={styles.link}>
                         <p className={styles.linkText}>View online</p>
                         <img src={externalLinkIcon} alt="" className={styles.linkIcon}/>
                     </a>
-                    <a href="" className={styles.link}>
+                    <a href={resume} download className={styles.link}>
                         <p className={styles.linkText}>Download PDF</p>
                         <img src={downloadIcon} alt="" className={styles.linkIcon}/>
                     </a>
