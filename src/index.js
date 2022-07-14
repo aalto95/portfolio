f212d434c95c96425d240a56705881ff16a62d89
aaltoo <scotlandfjord@protonmail.com>
2022-07-15 00:44:49 +0900

    Add react-i18n

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import store from './redux/store'
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
+import './i18n';
 
 ReactDOM.render(
     <BrowserRouter>
