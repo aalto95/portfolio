6399173ffb745a130678218b03504713ab6abc28
aaltoo <scotlandfjord@protonmail.com>
2022-10-15 19:26:48 +0900

    Fix path for projects component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-import "./components/projects/Projects";
-import "./components/Navbar";
 import Navbar from "./components/Navbar";
 import Contacts from "./components/Contacts";
 import Footer from "./components/Footer";
-import Projects from "./components/projects/Projects";
+import Projects from "./components/Projects/Projects";
 import About from "./components/About";
 import Resume from "./components/Resume";
 
-const App = (props) => {
+const App = () => {
   return (
     <div className="App">
       <Navbar />
